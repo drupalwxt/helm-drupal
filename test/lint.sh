9843b33a282d2635488adec18f3c6250f10a68d9
sylus <sylus1984@gmail.com>
2023-05-04 08:17:40 -0400

    feat(drupalwxt): Switch to chart releaser

--- test/lint.sh
+++ test/lint.sh
@@ -2,7 +2,16 @@
 
 set -euxo pipefail
 
+CHARTS="$@"
+CHART_TESTING_VERSION="3.7.0"
+
+if [ -z "$CHARTS" ]; then
+  CHARTS="--all"
+else
+  CHARTS="--charts $CHARTS"
+fi
+
 docker run --rm \
            --volume "$(pwd):/workdir" \
            --workdir /workdir \
-           "quay.io/helmpack/chart-testing:v3.1.1" ct lint --config test/config.yaml --all
+           "quay.io/helmpack/chart-testing:v${CHART_TESTING_VERSION}" ct lint --config test/config.yaml --lint-conf test/lintconf.yaml $CHARTS
